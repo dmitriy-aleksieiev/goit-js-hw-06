8d543e164b96fd5799b1012a13d9e8000ae055f2
Dmytro Aleksieiev <dmitriy.aleksieiev@gmail.com>
2024-04-01 07:59:53 +0300

    0104800

--- js/task-2.js
+++ js/task-2.js
@@ -14,7 +14,10 @@ class Storage {
   }
 
   removeItem(itemToRemove) {
-    this.#items.splice(this.#items.indexOf(itemToRemove), 1);
+    const index = this.#items.indexOf(itemToRemove);
+    if (index !== -1) {
+      this.#items.splice(index, 1);
+    }
   }
 }
 
